f964494b71a18cbcfbd3fe5bb8b91c3b8e3b9ad8
Gusthavo <gusthavogp123@gmail.com>
2026-06-26 22:01:41 -0300

    docs: atualização na documentao.txt dos novos endpoints.

--- smartcar/src/main/java/com/glc/smartcar/core/user/UsuarioService.java
+++ smartcar/src/main/java/com/glc/smartcar/core/user/UsuarioService.java
@@ -13,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 
 @Service
-public class UsuarioService {
+public class git UsuarioService {
 
     private final UserRepository userRepository;
     private final UsuarioMapper usuarioMapper;
