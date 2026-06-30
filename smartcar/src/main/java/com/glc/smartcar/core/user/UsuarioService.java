6c119f823933fc50920035dc2264fea5511b3988
Gusthavo <gusthavogp123@gmail.com>
2026-06-29 22:43:33 -0300

    fix: uma palavra solta atrapalhando o build

--- smartcar/src/main/java/com/glc/smartcar/core/user/UsuarioService.java
+++ smartcar/src/main/java/com/glc/smartcar/core/user/UsuarioService.java
@@ -13,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 
 @Service
-public class git UsuarioService {
+public class UsuarioService {
 
     private final UserRepository userRepository;
     private final UsuarioMapper usuarioMapper;
